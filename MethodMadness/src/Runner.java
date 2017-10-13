463ee95299df88ce4c4e59e5fb4936509920b312
Stanley <sliang5785@bths.edu>
2017-10-12 21:04:50 -0400

    Stanley Liang Method Madness

---
+++ MethodMadness/src/Runner.java
@@ -0,0 +1,10 @@
+// Stanley Liang Method Madness Due 10/12/17
+public class Runner 
+{
+	public static void main(String[] args)
+	{
+		System.out.println(MainMethod.isPalindrome("level"));
+		MainMethod.cutOut("abcd", "efgh");
+		System.out.println(MainMethod.isFibonacci(5));
+	}
+}
